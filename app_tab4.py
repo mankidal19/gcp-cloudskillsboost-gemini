32046791338784cd9fa0102f69733d49744ad72b
mankidal19 <man.kidal19@gmail.com>
2024-05-16 01:56:35 +0000

    Update the Image Playground tab - Video tags

--- app_tab4.py
+++ app_tab4.py
@@ -35,3 +35,32 @@ def render_video_playground_tab(multimodal_model_pro: GenerativeModel):
             st.write("Prompt used:")
             st.write(prompt,"\n","{video_data}")
 
+
+    with video_tags:
+        video_tags_uri = "gs://cloud-training/OCBL447/gemini-app/videos/photography.mp4"
+        video_tags_url = "https://storage.googleapis.com/"+video_tags_uri.split("gs://")[1]
+
+        video_tags_vid = Part.from_uri(video_tags_uri, mime_type="video/mp4")
+        st.video(video_tags_url)
+        st.write("Generate tags for the video.")
+
+        prompt = """Answer the following questions using the video only:
+                    1. What is in the video?
+                    2. What objects are in the video?
+                    3. What is the action in the video?
+                    4. Provide 5 best tags for this video?
+                    Write the answer in table format with the questions and answers in columns.
+                """
+
+        tab1, tab2 = st.tabs(["Response", "Prompt"])
+        video_tags_desc = st.button("Generate video tags", key="video_tags_desc")
+        with tab1:
+            if video_tags_desc and prompt: 
+                with st.spinner("Generating video tags"):
+                    response = get_gemini_pro_vision_response(multimodal_model_pro, [prompt, video_tags_vid])
+                    st.markdown(response)
+                    logging.info(response)
+        with tab2:
+            st.write("Prompt used:")
+            st.write(prompt,"\n","{video_data}")
+
